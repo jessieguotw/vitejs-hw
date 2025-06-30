4329f49498c96d685e9f3c932a77132d9565e0bc
jessieguotw <jessieguotw@gmail.com>
2025-06-30 11:15:23 +0800

    new action->copy

--- src/App.vue
+++ src/App.vue
@@ -42,11 +42,24 @@ onMounted(() => {
   }
 });
 
+//額外小功能 複製該欄網址
+const copyLink = (url) => {
+  navigator.clipboard
+    .writeText(url)
+    .then(() => {
+      alert('URL copied to clipboard!');
+    })
+    .catch((err) => {
+      console.error('Failed to copy URL:', err);
+    });
+};
 //
 </script>
 
 <template>
-  <div class="bg-slate-200 min-h-screen py-10">
+  <div
+    class="bg-slate-200 min-h-screen py-10 selection:bg-slate-600 selection:text-slate-100"
+  >
     <div class="max-w-3xl mx-auto p-6 mt-10 bg-white shadow-xl rounded-lg">
       <div>
         <div>
@@ -106,7 +119,7 @@ onMounted(() => {
                 <th
                   class="text-slate-700 px-4 py-2 w-[100px] text-center whitespace-nowrap text-sm"
                 >
-                  Delete
+                  Action
                 </th>
               </tr>
             </thead>
@@ -127,27 +140,48 @@ onMounted(() => {
                     🔗 {{ link.url }}
                   </a>
                 </td>
-                <td class="px-4 py-2 w-[100px] text-center whitespace-nowrap">
-                  <button
-                    @click="removeLink(link.id)"
-                    class="p-2 rounded hover-red-100 transition bg-red-100 hover:bg-red-200 cursor-pointer"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-Width="1.5"
-                      stroke="currentColor"
-                      c
-                      class="w-5 h-5 text-red-500 hover:text-red-700 transition"
+                <td class="px-2 py-2 w-[100px] text-center whitespace-nowrap">
+                  <div class="flex justify-center gap-2">
+                    <button
+                      @click="copyLink(link.url)"
+                      class="p-2 rounded hover-slate-100 transition bg-slate-200 hover:bg-slate-300 cursor-pointer"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-Width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5 text-slate-600 hover:text-slate-700 transition"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                        />
+                      </svg>
+                    </button>
+
+                    <button
+                      @click="removeLink(link.id)"
+                      class="p-2 rounded hover-red-100 transition bg-red-100 hover:bg-red-200 cursor-pointer"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-Width="1.5"
+                        stroke="currentColor"
+                        class="w-5 h-5 text-red-500 hover:text-red-700 transition"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
                 </td>
               </tr>
             </tbody>
